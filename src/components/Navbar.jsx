0a17553f853b9c6393cad2606c254c1aff0d26c5
Amy Gardiner <amymgardiner@gmail.com>
2023-02-02 16:26:50 -0600

    finished nav bar for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="flex flex-row justify-between py-8 mx-5">
+        <nav className="flex flex-row justify-between py-8 ml-12 mr-24">
             <h1 className="text-3xl">Jonah Walker</h1>
-            <ul className="flex flex-row space-x-6">
-                <Link to="/about" className="navbar--link">About</Link>
-                <Link to="/portfolio" className="navbar--link">Portfolio</Link>
-                <Link to="/contact" className="navbar--link">Contact</Link>
+            <ul className="flex flex-row space-x-16">
+                <Link to="/about" className="font-semibold navbar--link">About</Link>
+                <Link to="/portfolio" className="font-semibold navbar--link">Portfolio</Link>
+                <Link to="/contact" className="font-semibold navbar--link">Contact</Link>
             </ul>
         </nav>
     )

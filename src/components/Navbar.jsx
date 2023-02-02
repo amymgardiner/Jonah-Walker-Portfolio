f6c010606c0f38c29c374b5740cead3320a1cb07
Amy Gardiner <amymgardiner@gmail.com>
2023-02-02 16:19:13 -0600

    added nav bar hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="flex flex-row justify-between mx-5 py-8">
-            <h1>Jonah Walker</h1>
-            <ul className="flex flex-row space-x-4">
-                <Link to="/about" className="hover:bg-[#62929E]">About</Link>
-                <Link to="/portfolio" className="hover:bg-[#62929E]">Portfolio</Link>
-                <Link to="/contact" className="hover:bg-[#62929E]">Contact</Link>
+        <nav className="flex flex-row justify-between py-8 mx-5">
+            <h1 className="text-3xl">Jonah Walker</h1>
+            <ul className="flex flex-row space-x-6">
+                <Link to="/about" className="navbar--link">About</Link>
+                <Link to="/portfolio" className="navbar--link">Portfolio</Link>
+                <Link to="/contact" className="navbar--link">Contact</Link>
             </ul>
         </nav>
     )

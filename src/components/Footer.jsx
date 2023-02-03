b02d309e6b78556590cebfc56d09a0383bb311a9
Amy Gardiner <amymgardiner@gmail.com>
2023-02-03 15:39:45 -0600

    continued work on portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
     return (
         <footer className="flex flex-row justify-between py-8 footer">
-            <p className="ml-40 text-sm">©2023 Jonah Walker</p>
+            <p className="m-auto ml-40 text-sm">©2023 Jonah Walker</p>
             <div className="flex flex-row mr-40 space-x-16">
                 <Link to="https://www.youtube.com/@jonahwalker4425" target="_blank" className="text-4xl hover:text-[#62929E]"><TfiYoutube/></Link>
                 <Link to="https://www.linkedin.com/in/jonahkwalker" target="_blank" className="text-4xl hover:text-[#62929E]"><AiFillLinkedin/></Link>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,78 +1,115 @@
 import React from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 const Portfolio  = () => {
     return (
         <div>
             <Navbar/>
-            <div className="main">
-                <div className="">
-                <h2 className="text-2xl">Z-CAM Anamorphic Footage</h2>
-                    <Carousel className="max-w-[800px] max-h-[450px]">
-                        <iframe
-                        width="720"
-                        height="405"
-                        src="https://www.youtube.com/embed/Zv89Gnqy-Io"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
+                <div className="flex flex-col items-center portfolio--main">
+                    <div className="py-24 my-8 portfolio--main_slider">
+                        <div className="flex flex-row justify-between">
+                            <h2 className="mb-4 text-2xl">Z-CAM Anamorphic Footage</h2>
+                            <div className="flex flex-col">
+                                <p className="">Camera - Z CAM E2-M4</p>
+                                <p className="">Lens(es) - Vazen 28mm 1.8x Anamorphic</p>
+                            </div>
+                        </div>
+                        <Carousel infiniteLoop={true} showStatus={false} className="max-w-[1200px] max-h-[675px] bg-[#4F5D75]">
+                            <iframe
+                            width="950"
+                            height="534"
+                            src="https://www.youtube.com/embed/Zv89Gnqy-Io"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
 
-                        <iframe
-                        width="720"
-                        height="405"
-                        src="https://www.youtube.com/embed/p0znG6E5ZYY"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
+                            <iframe
+                            width="950"
+                            height="534"
+                            src="https://www.youtube.com/embed/p0znG6E5ZYY"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
 
-                        <iframe
-                        width="720"
-                        height="405"
-                        src="https://www.youtube.com/embed/ovIIbmeXsxw"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-                    </Carousel>
+                            <iframe
+                            width="950"
+                            height="534"
+                            src="https://www.youtube.com/embed/ovIIbmeXsxw"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+                        </Carousel>
+                    </div>
+                    <div className="bg-[#EF8354] w-9/12 h-px"></div>
+
+                    <div className="py-24 my-8 portfolio--main_slider">
+                        <div className="flex flex-row justify-between">
+                            <h2 className="mb-4 text-2xl">Z-CAM Footage</h2>
+                            <div className="flex flex-col">
+                                <p className="">Camera - Z CAM E2-M4</p>
+                                <p className="">Lens(es) - DZOFILM Vespid Prime 40mm</p>
+                                <p className="">Lens(es) -  Rokinon 20mm & 35mm Cine DS</p>
+                            </div>
+                        </div>
+                        <Carousel infiniteLoop={true} showStatus={false} className="max-w-[1200px] max-h-[675px] bg-[#4F5D75]">
+                            <iframe
+                            width="950"
+                            height="534"
+                            src="https://www.youtube.com/embed/l7I59jAKask"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+
+                            <iframe
+                            width="950"
+                            height="534"
+                            src="https://www.youtube.com/embed/QmYJndRew6E"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+
+                            <iframe
+                            width="950"
+                            height="534"
+                            src="https://www.youtube.com/embed/vGBZTUbv9_o"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+                        </Carousel>
+                    </div>
+                    <div className="bg-[#EF8354] w-9/12 h-px"></div>
+
+                    <div className="py-24 my-8 portfolio--main_slider">
+                        <div className="flex flex-row justify-between">
+                            <h2 className="mb-4 text-2xl">Fujifilm Footage</h2>
+                            <div className="flex flex-col">
+                                <p className="">Camera - Fuji XT-30</p>
+                                <p className="">Lens(es) -  Rokinon 20mm & 35mm Cine DS</p>
+                            </div>
+                        </div>
+                        <Carousel infiniteLoop={true} showStatus={false} className="max-w-[1200px] max-h-[675px] bg-[#4F5D75]">
+                            <iframe
+                            width="950"
+                            height="534"
+                            src="https://www.youtube.com/embed/5Zed_gZwQEM"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+                        </Carousel>
+                    </div>
+                    <div className="bg-[#EF8354] w-9/12 h-px"></div>
                 </div>
-
-                <div className="">
-                <h2 className="text-2xl">Z-CAM E2 M4 Footage</h2>
-                    <Carousel className="max-w-[800px] max-h-[450px]">
-                        <iframe
-                        width="720"
-                        height="405"
-                        src="https://www.youtube.com/embed/l7I59jAKask"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-
-                        <iframe
-                        width="720"
-                        height="405"
-                        src="https://www.youtube.com/embed/QmYJndRew6E"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-
-                        <iframe
-                        width="720"
-                        height="405"
-                        src="https://www.youtube.com/embed/vGBZTUbv9_o"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen></iframe>
-                    </Carousel>
-                </div>
-            </div>
             <Footer/>
         </div>
     )

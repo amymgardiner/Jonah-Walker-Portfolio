416984c7361985306b7b327d0f00a84bce7499f5
Amy Gardiner <amymgardiner@gmail.com>
2023-02-06 17:36:59 -0600

    starting on media breakpoints - done for landing page

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -5,16 +5,16 @@ import HomeImage from '../assets/img/kirk-thornton-x9btTuM_F04-unsplash.jpg';
 const Site = () => {
     return (
         <div className= "w-screen h-screen bg-center bg-cover site--main" style={{ backgroundImage:`url(${HomeImage})` }}>
-            <div className="flex flex-col ml-40 site--main_title">
-                <h1 className="text-8xl mt-44">Jonah Walker</h1>
+            <div className="flex flex-col ml-[15%] site--main_title">
+                <h1 className="text-9xl mt-[20%] site--main_title-name">Jonah Walker</h1>
                 <ul className="flex flex-col mt-2">
-                    <li className="text-3xl">Cinematographer</li>
-                    <li className="text-3xl">Video Editor</li>
-                    <li className="text-3xl">Colorist</li>
+                    <li className="text-4xl site--main_title-description">Cinematographer</li>
+                    <li className="text-4xl site--main_title-description">Video Editor</li>
+                    <li className="text-4xl site--main_title-description">Colorist</li>
                 </ul>
             </div>
-            <div className="mt-20 ml-40">
-                <button className="px-8 py-3 text-2xl rounded-lg site--main_button"><Link to="/home">Explore</Link></button>
+            <div className="mt-20 ml-[15%]">
+                <button className="px-8 py-3 text-3xl rounded-lg site--main_button"><Link to="/home">Explore</Link></button>
             </div>
         </div>
     )

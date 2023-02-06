711d1f5c6f731da99235e28da9cb6fc57894de27
Amy Gardiner <amymgardiner@gmail.com>
2023-02-06 15:30:23 -0600

    finished hover effects on video still images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link} from "react-router-dom";
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Gallery1 from '../assets/img/test screenshot 1.png'
@@ -12,44 +13,50 @@ const Portfolio  = () => {
     return (
         <div>
             <Navbar/>
-                <section class="overflow-hidden">
-                    <div class="container px-5 py-2 mx-auto">
-                        <div class="flex flex-wrap">
-                            <div class="flex flex-wrap w-1/3">
-                                <div class="w-full p-1">
-                                    <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg aspect-video"
+                <section className="overflow-hidden">
+                    <div className="container px-5 py-2 mx-auto">
+                        <div className="flex flex-wrap">
+                            <div className="flex flex-wrap w-1/3">
+                                <Link to="https://youtu.be/Zv89Gnqy-Io" target="_blank"><div className="relative w-full p-1 text-center portfolio--image">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg aspect-video"
                                     src={Gallery1}/>
-                                </div>
+                                    <h2 className="absolute text-xl top-1/2 left-1/2 portfolio--image_title">Seven Bridges Trail</h2>
+                                </div></Link>
                             </div>
-                            <div class="flex flex-wrap w-1/3">
-                                <div class="w-full p-1">
-                                    <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg aspect-video"
+                            <div className="flex flex-wrap w-1/3">
+                                <Link to="https://youtu.be/p0znG6E5ZYY" target="_blank"><div className="relative w-full p-1 text-center portfolio--image">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg aspect-video"
                                     src={Gallery2}/>
-                                </div>
+                                    <h2 className="absolute text-xl top-1/2 left-1/2 portfolio--image_title">Great Sand Dunes National Park and Preserve</h2>
+                                </div></Link>
                             </div>
-                            <div class="flex flex-wrap w-1/3">
-                                <div class="w-full p-1">
-                                    <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg aspect-video"
+                            <div className="flex flex-wrap w-1/3">
+                                <Link to="https://youtu.be/ovIIbmeXsxw" target="_blank"><div className="relative w-full p-1 text-center portfolio--image">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg aspect-video"
                                     src={Gallery3}/>
-                                </div>
+                                    <h2 className="absolute text-xl top-1/2 left-1/2 portfolio--image_title">Monument Rocks</h2>
+                                </div></Link>
                             </div>
-                            <div class="flex flex-wrap w-1/3">
-                                <div class="w-full p-1">
-                                    <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg aspect-video"
+                            <div className="flex flex-wrap w-1/3">
+                                <Link to="https://youtu.be/l7I59jAKask" target="_blank"><div className="relative w-full p-1 text-center portfolio--image">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg aspect-video"
                                     src={Gallery4}/>
-                                </div>
+                                    <h2 className="absolute text-xl top-1/2 left-1/2 portfolio--image_title">Great Forest Park Balloon Race</h2>
+                                </div></Link>
                             </div>
-                            <div class="flex flex-wrap w-1/3">
-                                <div class="w-full p-1">
-                                    <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg aspect-video"
+                            <div className="flex flex-wrap w-1/3">
+                                <Link to="https://youtu.be/QmYJndRew6E" target="_blank"><div className="relative w-full p-1 text-center portfolio--image">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg aspect-video"
                                     src={Gallery5}/>
-                                </div>
+                                    <h2 className="absolute text-xl top-1/2 left-1/2 portfolio--image_title">Saturday Barbeque</h2>
+                                </div></Link>
                             </div>
-                            <div class="flex flex-wrap w-1/3">
-                                <div class="w-full p-1">
-                                    <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg aspect-video"
+                            <div className="flex flex-wrap w-1/3">
+                                <Link to="https://youtu.be/vGBZTUbv9_o" target="_blank"><div className="relative w-full p-1 text-center portfolio--image">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg aspect-video"
                                     src={Gallery6}/>
-                                </div>
+                                    <h2 className="absolute text-xl top-1/2 left-1/2 portfolio--image_title">Castlewood State Park</h2>
+                                </div></Link>
                             </div>
                         </div>
                     </div>

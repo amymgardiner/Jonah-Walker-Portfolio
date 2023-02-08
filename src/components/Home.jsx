97fca0d6af620107ffb7de31229df98ddf5aa318
Amy Gardiner <amymgardiner@gmail.com>
2023-02-08 16:05:04 -0600

    starting on homepage media breakpoints

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,15 @@ const Home = () => {
     return (
         <div>
             <Navbar/>
-            <section className="overflow-hidden main--body">
+            <section className="flex flex-col items-center justify-center overflow-hidden main--body">
                 <div className="container flex flex-col items-center justify-center main--body">
 
-                    <div className="py-24 my-8 main--body_videos">
-                        <div className="flex flex-row justify-center mb-4 space-x-36">
+                    <div className="flex flex-col justify-center py-24 my-8 main--body_videos">
+                        <div className="flex flex-row justify-center mb-4 space-x-36 home--videos_title">
                             <h2 className="text-2xl font-semibold">The Story of 'Tis the Season</h2>
-                            <p className="text-[#BFC0C0] max-w-[40%]">In this short film, for Kaldi's Coffee, I take a behind-the-scenes look at the people and vintage machines that bring 'Tis the Season holiday blend to life each year.</p>
+                            <p className="text-[#BFC0C0] max-w-[40%] home--videos_title-description">In this short film, for Kaldi's Coffee, I take a behind-the-scenes look at the people and vintage machines that bring 'Tis the Season holiday blend to life each year.</p>
                         </div>
-                        <div className="flex justify-center">
+                        <div className="flex justify-center home--videos">
                         <iframe
                         width="1000"
                         height="563"
@@ -28,7 +28,7 @@ const Home = () => {
 
                     <div className="bg-[#6c757d] w-9/12 h-0.5"></div>
 
-                    <div className="py-24 my-8 main--body_videos">
+                    <div className="flex flex-col justify-center py-24 my-8 main--body_videos">
                         <div className="flex flex-row justify-center mb-8">
                             <h2 className="text-5xl font-bold">Reel</h2>
                         </div>
